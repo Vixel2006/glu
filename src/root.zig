1f8220fef1ab176e97abd343edbcbad9500b8e53
Yusuf Shihata <yusufshihata2006@gmail.com>
2026-06-18 12:29:45 +0300

    finishing the simple zig code generator for IDL

--- src/root.zig
+++ src/root.zig
@@ -4,6 +4,7 @@ pub const Channel = @import("channel.zig").Channel;
 pub const write = @import("channel.zig").write;
 pub const read = @import("channel.zig").read;
 pub const parser = @import("codegen/parser.zig");
+pub const generator = @import("codegen/generator.zig");
 pub const Publisher = @import("api/publisher.zig").Publisher;
 pub const Subscriber = @import("api/subscriber.zig").Subscriber;
 pub const Node = @import("api/node.zig").Node;
@@ -11,6 +12,7 @@ pub const Node = @import("api/node.zig").Node;
 comptime {
     _ = @import("topic.zig");
     _ = @import("codegen/parser.zig");
+    _ = @import("codegen/generator.zig");
     _ = @import("channel.zig");
     _ = @import("api/publisher.zig");
     _ = @import("api/subscriber.zig");
